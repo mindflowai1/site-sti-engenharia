c83ca29eadc72d177825579ffa30103d092aa150
mindflowai1 <mindflow.ai.tests@gmail.com>
2026-04-01 18:22:24 -0300

    incendio

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        incendio: resolve(__dirname, 'incendio/index.html'),
+        lp3: resolve(__dirname, 'lp3/index.html'),
+      },
+    },
+  },
 });
